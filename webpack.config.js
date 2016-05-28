bb3a9c358492043df992d94430d4b37c676a6efc
Abimbola Idowu <abimbola2010@gmail.com>
2016-05-28 18:34:01 +0100

    Initial commit
    * Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const fs      = require('fs');
+const path    = require('path'),
+      join    = path.join,
+      resolve = path.resolve;
+
+const getConfig = require('hjs-webpack');
+const root    = resolve(__dirname);
+const src     = join(root, 'src');
+const modules = join(root, 'node_modules');
+const dest    = join(root, 'dist');
+
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+
+var config = getConfig({
+  isDev: isDev,
+  in: join(src, 'app.js'),
+  out: dest,
+  clearBeforeBuild: true
+});
+
+module.exports = config;
